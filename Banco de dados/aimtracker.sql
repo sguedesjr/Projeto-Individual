8e40babd9967eb7865f30dc33f90b73772740451
sguedesjr <sandro.guedesjr@sptech.school>
2025-05-17 02:55:42 -0300

    Adicionando pasta de banco de dados finalizada

---
+++ Banco de dados/aimtracker.sql
@@ -0,0 +1,29 @@
+create database aimtracker;
+use aimtracker;
+
+CREATE TABLE usuario (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(30) NOT NULL,
+    email VARCHAR(45) NOT NULL,
+    senha VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE estatisticas (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    pontuacao INT,
+    erros INT,
+    acertos INT
+);
+
+
+CREATE TABLE leaderboard(
+id INT,
+fkUsuario INT,
+fkEstatisticas INT,
+constraint pkComposta primary key(id, fkUsuario, fkEstatisticas),
+constraint fkUsuario foreign key leaderboard(fkUsuario) references usuario(id),
+constraint fkEstatisticas foreign key leaderboard(fkEstatisticas) references estatisticas(id)
+)auto_increment=1;
+
+select * from usuario;
+
